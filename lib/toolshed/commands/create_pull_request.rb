6072db9f20eeedf7840f5ca695f3af0dcccef3e7
wallerjake <wallerjake@gmail.com>
2014-11-26 15:27:24 -0600

    Refactoring create pull request class

--- lib/toolshed/commands/create_pull_request.rb
+++ lib/toolshed/commands/create_pull_request.rb
@@ -1,182 +1,189 @@
 module Toolshed
   module Commands
     class CreatePullRequest
-      def execute(args, options = {})
-        # see what branch is checked out and where we are branched from
-        puts "Current Branch: #{Toolshed::Git::Base.branch_name}"
-        puts "Branched From: #{Toolshed::Git::Base.branched_from}"
-        puts "Using Defaults: #{(Toolshed::Client.use_defaults.nil?) ? 'No' : 'Yes'}"
+      attr_accessor :ticket_tracking_url, :ticket_tracking_title,
+                    :ticket_id, :ticket_tracker,
+                    :ticket_tracker_class, :ticket_tracker_project_id,
+                    :pull_request_url, :git_tool,
+                    :pull_request_title, :pull_request_body
 
-        unless (Toolshed::Client.ticket_tracking_tool.nil? || Toolshed::Client.ticket_tracking_tool.empty?)
-          ticket_tracking_url = ''
-          ticket_tracking_title = ''
-          ticket_id = ''
-
-          begin
-            ticket_tracker_class =  Object.const_get("Toolshed::TicketTracking::#{Toolshed::Client.ticket_tracking_tool.camel_case}")
-
-            use_project_id = Object.const_get("#{ticket_tracker_class}::USE_PROJECT_ID") rescue false
-            if use_project_id
-              ticket_tracker_project_id = read_user_input_project(
-                "Project ID (Default: #{Toolshed::Client.default_pivotal_tracker_project_id}):",
-                options.merge!({
-                  default: Toolshed::Client.default_pivotal_tracker_project_id,
-                })
-              )
-              options.merge!({
-                project_id: ticket_tracker_project_id,
-              })
-            end
-
-            use_project_name = Object.const_get("#{ticket_tracker_class}::USE_PROJECT_NAME") rescue false
-            if use_project_name
-              ticket_tracker_project_name = read_user_input_project(
-                "Project Name (Default: #{Toolshed::Client.default_ticket_tracker_project}):", options.merge!({
-                  default: Toolshed::Client.default_ticket_tracker_project,
-                })
-              )
-              options.merge!({
-                project: ticket_tracker_project_name,
-              })
-            end
-
-            # @TODO - refactor this code into the git module seems more appropriate since it's performing git functions
-            ticket_id = read_user_input_ticket_tracker_ticket_id(
-              "Ticket ID (Default: #{Toolshed::TicketTracking::story_id_from_branch_name(Toolshed::Git::Base.branch_name)}):", {
-                default: Toolshed::TicketTracking::story_id_from_branch_name(Toolshed::Git::Base.branch_name),
-              }
-            )
-            options.merge!({
-              ticket_id: ticket_id,
-            })
-
-            ticket_tracker = ticket_tracker_class.create_instance(options)
-
-            ticket_tracking_url = ticket_tracker.url
-            ticket_tracking_title = ticket_tracker.default_title
-            ticket_id = ticket_id
-
-            puts "Ticket Tracking URL: #{ticket_tracking_url}"
-            puts "Ticket Tracking title: #{ticket_tracking_title}"
-            puts "Ticket ID: #{ticket_id}"
-          rescue Exception => e
-            puts e.inspect
-            puts "Ticket tracking tool is not supported at this time"
-            return
-          end
-        end
-
-        pull_request_url = ''
-        begin
-          git_tool_class = Object.const_get("Toolshed::Git::#{Toolshed::Client.git_tool.camel_case}")
-          git_tool = git_tool_class.create_instance
-
-          # create the pull request prompt when needed
-          title = read_user_input_pull_request_title(
-            "Pull request title (Default: #{ticket_tracking_title}):",
-            options.merge!({
-              default: ticket_tracking_title,
-            })
-          )
-          body = read_user_input_pull_request_body(
-            "Pull request body (Default: #{ticket_tracking_url}):",
-            options.merge!({
-              default: ticket_tracking_url
-            })
-          )
-
-          puts "Pull request being created"
-          git_pull_request_result = git_tool.create_pull_request(title, body)
-          pull_request_url = git_pull_request_result["html_url"]
-
-          unless (Toolshed::Client.ticket_tracking_tool.nil? || Toolshed::Client.ticket_tracking_tool.empty?)
-            add_note_to_ticket = read_user_input_add_note_to_ticket("Would you like to add a note with the pull request url?")
-            if (add_note_to_ticket)
-              result = ticket_tracker.add_note(pull_request_url)
-
-              default_completed_status = Object.const_get("#{ticket_tracker_class}::DEFAULT_COMPLETED_STATUS") rescue false
-              unless (default_completed_status)
-                default_completed_status = Toolshed::Client.ticket_status_for_complete
-              end
-
-              result = ticket_tracker.update_ticket_status(default_completed_status)
-            end
-          end
-
-          puts "Created Pull Request: #{pull_request_url}"
-        rescue => e
-          puts e.message
-          exit
-        end
+      def initialize(options={})
+        self.ticket_tracker_class = nil
+        self.pull_request_url = ''
       end
 
-      def read_user_input_add_note_to_ticket(message)
-        return true if (Toolshed::Client.use_defaults)
+      def execute(args, options = {})
+        output_begining_messages
+        options = execute_ticket_tracking(options)
+        options = execute_pull_request(options) unless options.nil?
+      end
 
-        puts message
-        value = $stdin.gets.chomp
+      private
 
-        until (%w(y n).include?(value.downcase))
-          puts "Value must be Y or N"
+        def read_user_input_add_note_to_ticket(message)
+          return true if (Toolshed::Client.use_defaults)
+
           puts message
           value = $stdin.gets.chomp
+
+          until %w(y n).include?(value.downcase)
+            puts "Value must be Y or N"
+            puts message
+            value = $stdin.gets.chomp
+          end
+
+          (value == 'y') ? true : false
         end
 
-        (value == 'y') ? true : false
-      end
+        def read_user_input(message, options)
+          return options[:default] if Toolshed::Client.use_defaults
+          return options[:title] if options.has_key?(:title)
+          return options[:body] if options.has_key?(:body)
 
-      def read_user_input_pull_request_title(message, options)
-        return options[:title] if (options.has_key?(:title))
-        return options[:default] if (Toolshed::Client.use_defaults)
-
-        puts message
-        value = $stdin.gets.chomp
-        if (value.empty?)
-          value = options[:default]
+          puts message
+          value = $stdin.gets.chomp
+          value = options[:default] if value.empty?
+          value
         end
 
-        value
-      end
-
-      def read_user_input_pull_request_body(message, options)
-        return options[:body] if (options.has_key?(:body))
-        return options[:default] if (Toolshed::Client.use_defaults)
-
-        puts message
-        value = $stdin.gets.chomp
-
-        if (value.empty?)
-          value = options[:default]
+        def read_user_input_ticket_tracker_ticket_id(message, options)
+          read_user_input(message, options)
         end
 
-        value
-      end
-
-      def read_user_input_project(message, options)
-        return options[:default] if (Toolshed::Client.use_defaults)
-
-        puts message
-        value = $stdin.gets.chomp
-
-        if (value.empty?)
-          value = options[:default]
+        def output_begining_messages
+          puts "Current Branch: #{Toolshed::Git::Base.branch_name}"
+          puts "Branched From: #{Toolshed::Git::Base.branched_from}"
+          puts "Using Defaults: #{(Toolshed::Client.use_defaults.nil?) ? 'No' : 'Yes'}"
         end
 
-        value
-      end
-
-      def read_user_input_ticket_tracker_ticket_id(message, options)
-        return options[:default] if (Toolshed::Client.use_defaults)
-
-        puts message
-        value = $stdin.gets.chomp
-
-        if (value.empty?)
-          value = options[:default]
+        def use_ticket_tracker_project_id(options)
+          use_project_id = Object.const_get("#{ticket_tracker_class}::USE_PROJECT_ID") rescue false
+          if use_project_id
+            ticket_tracker_project_id = read_user_input(
+              "Project ID (Default: #{Toolshed::Client.default_pivotal_tracker_project_id}):",
+              options.merge!({
+                default: Toolshed::Client.default_pivotal_tracker_project_id,
+              })
+            )
+            options.merge!({
+              project_id: ticket_tracker_project_id,
+            })
+          end
+          options
         end
 
-        value
-      end
+        def use_ticket_tracker_project_name(options)
+          use_project_name = Object.const_get("#{ticket_tracker_class}::USE_PROJECT_NAME") rescue false
+          if use_project_name
+            ticket_tracker_project_name = read_user_input(
+              "Project Name (Default: #{Toolshed::Client.default_ticket_tracker_project}):", options.merge!({
+                default: Toolshed::Client.default_ticket_tracker_project,
+              })
+            )
+            options.merge!({
+              project: ticket_tracker_project_name,
+            })
+          end
+          options
+        end
+
+        def get_ticket_id(options)
+          self.ticket_id = read_user_input_ticket_tracker_ticket_id(
+            "Ticket ID (Default: #{Toolshed::TicketTracking::story_id_from_branch_name(Toolshed::Git::Base.branch_name)}):", {
+              default: Toolshed::TicketTracking::story_id_from_branch_name(Toolshed::Git::Base.branch_name),
+            }
+          )
+          options.merge!({
+            ticket_id: self.ticket_id,
+          })
+          options
+        end
+
+        def output_ticket_information
+          puts "Ticket Tracking URL: #{ticket_tracking_url}"
+          puts "Ticket Tracking title: #{ticket_tracking_title}"
+          puts "Ticket ID: #{ticket_id}"
+        end
+
+        def execute_ticket_tracking(options)
+          unless Toolshed::Client.ticket_tracking_tool.nil? || Toolshed::Client.ticket_tracking_tool.empty?
+            begin
+              self.ticket_tracker_class =  Object.const_get("Toolshed::TicketTracking::#{Toolshed::Client.ticket_tracking_tool.camel_case}")
+
+              options = use_ticket_tracker_project_id(options)
+              options = use_ticket_tracker_project_name(options)
+              options = get_ticket_id(options)
+              self.ticket_tracker = ticket_tracker_class.create_instance(options)
+              self.ticket_tracking_url = ticket_tracker.url
+              self.ticket_tracking_title = ticket_tracker.default_title
+              output_ticket_information
+            rescue Exception => e
+              puts e.inspect
+              puts e.backtrace
+              puts "Ticket tracking tool is not supported at this time"
+              return
+            end
+            options
+          end
+          options
+        end
+
+        def add_note_to_ticket
+          add_note_to_ticket_response = read_user_input_add_note_to_ticket(
+            "Would you like to add a note with the pull request url?"
+          )
+          if add_note_to_ticket_response
+            result = self.ticket_tracker.add_note(pull_request_url)
+            default_completed_status = Object.const_get("#{ticket_tracker_class}::DEFAULT_COMPLETED_STATUS") rescue false
+            default_completed_status = Toolshed::Client.ticket_status_for_complete unless default_completed_status
+            ticket_tracker.update_ticket_status(default_completed_status)
+          end
+        end
+
+        def pull_request_created_message
+          puts "Created Pull Request: #{pull_request_url}"
+        end
+
+        def execute_pull_request(options)
+          begin
+            self.git_tool = Object.const_get("Toolshed::Git::#{Toolshed::Client.git_tool.camel_case}").create_instance
+
+            options = set_pull_request_title(options)
+            options = set_pull_request_body(options)
+            send_pull_request
+            add_note_to_ticket unless ticket_tracker_class.nil?
+            pull_request_created_message
+          rescue => e
+            puts e.message
+            exit
+          end
+        end
+
+        def set_pull_request_title(options)
+          self.pull_request_title = read_user_input(
+            "Pull request title (Default: #{self.ticket_tracking_title}):",
+            options.merge!({
+              default: self.ticket_tracking_title,
+            })
+          )
+          options
+        end
+
+        def set_pull_request_body(options)
+          self.pull_request_body = read_user_input(
+            "Pull request body (Default: #{self.ticket_tracking_url}):",
+            options.merge!({
+              default: self.ticket_tracking_url
+            })
+          )
+          options
+        end
+
+        def send_pull_request
+          puts "Pull request being created"
+          git_pull_request_result = self.git_tool.create_pull_request('Sample', 'Sample Body')
+          self.pull_request_url = git_pull_request_result["html_url"]
+        end
     end
   end
 end
