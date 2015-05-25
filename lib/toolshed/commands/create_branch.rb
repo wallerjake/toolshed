8ea248d47202ed8b9291f21fc5a9fa6c9956a369
Jake Waller <wallerjake@gmail.com>
2015-05-25 17:16:46 -0500

    Clean up the create_branch command of rubocop errors

--- lib/toolshed/commands/create_branch.rb
+++ lib/toolshed/commands/create_branch.rb
@@ -1,12 +1,13 @@
 module Toolshed
   module Commands
+    # Create a branch from a given set of parameters
     class CreateBranch
-      def self.cli_options
+      def self.cli_options # rubocop:disable Metrics/MethodLength
         {
           banner: 'Usage: create_branch [options]',
           options: {
             branch_name: {
-              short_on: '-b',
+              short_on: '-b'
             },
             branch_from: {
               short_on: '-f'
@@ -15,24 +16,19 @@ module Toolshed
         }
       end
 
-      def execute(args, options = {})
-        begin
-          branch_name = read_user_input_branch_name("Branch name:", options)
-          branch_from = read_user_input_branch_from("Branch from:", options)
+      def execute(_args, options = {}) # rubocop:disable Metrics/MethodLength
+        branch_name = read_user_input_branch_name('Branch name:', options)
+        branch_from = read_user_input_branch_from('Branch from:', options)
 
-          git = Toolshed::Git::Base.new({
-            from_remote_branch_name: branch_from,
-            to_remote_branch_name: branch_name,
-          })
-          git.create_branch
-          Toolshed.die
-        rescue Veto::InvalidEntity => e
-          puts "Unable to create branch due to the following errors"
-          e.message.each do |key, value|
-            puts "#{key}: #{value}"
-          end
-          Toolshed.die
+        git = Toolshed::Git::Base.new(from_remote_branch_name: branch_from, to_remote_branch_name: branch_name) # rubocop:disable Metrics/LineLength
+        git.create_branch
+        Toolshed.die
+      rescue Veto::InvalidEntity => e
+        Toolshed.logger.fatal 'Unable to create branch due to the following errors' # rubocop:disable Metrics/LineLength
+        e.message.each do |key, value|
+          Toolshed.logger.fatal "#{key}: #{value}"
         end
+        Toolshed.die
       end
 
       def read_user_input_branch_name(message, options)
@@ -41,8 +37,8 @@ module Toolshed
         puts message
         value = $stdin.gets.chomp
 
-        until (!value.empty?)
-          puts "Branch name cannot be empty"
+        while value.empty?
+          puts 'Branch name cannot be empty'
           puts message
           value = $stdin.gets.chomp
         end
@@ -51,7 +47,7 @@ module Toolshed
       end
 
       def read_user_input_branch_from(message, options)
-        return options[:branch_from] if (options.has_key?(:branch_from))
+        return options[:branch_from] if options.key?(:branch_from)
 
         # if branch-name was supplied then default to master if not supplied
         return Toolshed::Git::DEFAULT_BRANCH_FROM if options.key?(:branch_name)
@@ -59,8 +55,8 @@ module Toolshed
         puts message
         value = $stdin.gets.chomp
 
-        until (!value.empty?)
-          puts "Branch from cannot be empty"
+        while value.empty?
+          puts 'Branch from cannot be empty'
           puts message
           value = $stdin.gets.chomp
         end
--- toolshed.gemspec
+++ toolshed.gemspec
@@ -38,4 +38,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency 'ci_reporter'
   spec.add_development_dependency 'simplecov'
   spec.add_development_dependency 'codeclimate-test-reporter'
+  spec.add_development_dependency 'rubocop'
 end
