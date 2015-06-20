2b8f6e744e27a28afb0ca9accf0b088832c8abc3
Jake Waller <wallerjake@gmail.com>
2015-06-20 16:54:49 -0500

    Additional rubocop cleanup

--- lib/toolshed/client.rb
+++ lib/toolshed/client.rb
@@ -3,6 +3,7 @@ require 'toolshed/version'
 require 'yaml'
 
 module Toolshed
+  # This is responsible for loading .toolshedrc file
   class Client
     include Singleton
 
@@ -39,56 +40,55 @@ module Toolshed
                   :log_path,
                   :credentials_loaded_from
 
-    GITHUB_BASE_API_URL  = "https://api.github.com/"
-    PIVOTAL_TRACKER_BASE_API_URL = "https://www.pivotaltracker.com/services/v5/"
+    GITHUB_BASE_API_URL  = 'https://api.github.com/'
+    PIVOTAL_TRACKER_BASE_API_URL = 'https://www.pivotaltracker.com/services/v5/'
 
     class << self
       def config_path
         ENV['TOOLSHED_CONFIG'] || '~/.toolshedrc'
       end
 
-      def load_credentials(path = config_path)
-        credentials = Client.read_credenials(config_path)
-        instance.github_username                    ||= credentials['github_username']
-        instance.github_password                    ||= credentials['github_password']
-        instance.github_token                       ||= credentials['github_token']
-        instance.pivotal_tracker_username           ||= credentials['pivotal_tracker_username']
-        instance.pivotal_tracker_password           ||= credentials['pivotal_tracker_password']
-        instance.default_pivotal_tracker_project_id ||= credentials['default_pivotal_tracker_project_id']
-        instance.ticket_tracker_username            ||= credentials['ticket_tracker_username']
-        instance.ticket_tracker_password            ||= credentials['ticket_tracker_password']
-        instance.ticket_tracker_owner               ||= credentials['ticket_tracker_owner']
-        instance.ticket_status_for_complete         ||= credentials['ticket_status_for_complete']
-        instance.default_ticket_tracker_project     ||= credentials['default_ticket_tracker_project']
-        instance.pull_from_remote_name              ||= credentials['pull_from_remote_name']
-        instance.pull_from_repository_user          ||= credentials['pull_from_repository_user']
-        instance.pull_from_repository_name          ||= credentials['pull_from_repository_name']
-        instance.push_to_repository_user            ||= credentials['push_to_repository_user']
-        instance.push_to_remote_name                ||= credentials['push_to_remote_name']
-        instance.ticket_tracking_tool               ||= credentials['ticket_tracking_tool']
-        instance.use_git_submodules                 ||= credentials['use_git_submodules']
-        instance.git_tool                           ||= credentials['git_tool']
-        instance.time_tracking_username             ||= credentials['time_tracking_username']
-        instance.time_tracking_password             ||= credentials['time_tracking_password']
-        instance.time_tracking_owner                ||= credentials['time_tracking_owner']
-        instance.time_tracking_default_project_id   ||= credentials['time_tracking_default_project_id']
-        instance.time_tracking_tool                 ||= credentials['time_tracking_tool']
-        instance.git_quiet                          ||= (credentials['git_quiet']) ? '&> /dev/null' : ''
-        instance.use_defaults                       ||= credentials['use_defaults']
-        instance.default_pull_request_title_format  ||= credentials['default_pull_request_title_format']
-        instance.log_path                           ||= credentials['log_path']
-        instance.credentials_loaded_from            ||= load_credentials_from(config_path)
+      def load_credentials # rubocop:disable Metrics/AbcSize, Metrics/MethodLength
+        credentials = Client.read_credenials
+        instance.github_username ||= credentials['github_username']
+        instance.github_password ||= credentials['github_password']
+        instance.github_token ||= credentials['github_token']
+        instance.pivotal_tracker_username ||= credentials['pivotal_tracker_username']
+        instance.pivotal_tracker_password ||= credentials['pivotal_tracker_password']
+        instance.default_pivotal_tracker_project_id ||= credentials['default_pivotal_tracker_project_id'] # rubocop:disable Metrics/LineLength
+        instance.ticket_tracker_username ||= credentials['ticket_tracker_username']
+        instance.ticket_tracker_password ||= credentials['ticket_tracker_password']
+        instance.ticket_tracker_owner ||= credentials['ticket_tracker_owner']
+        instance.ticket_status_for_complete ||= credentials['ticket_status_for_complete']
+        instance.default_ticket_tracker_project ||= credentials['default_ticket_tracker_project'] # rubocop:disable Metrics/LineLength
+        instance.pull_from_remote_name ||= credentials['pull_from_remote_name']
+        instance.pull_from_repository_user ||= credentials['pull_from_repository_user']
+        instance.pull_from_repository_name ||= credentials['pull_from_repository_name']
+        instance.push_to_repository_user ||= credentials['push_to_repository_user']
+        instance.push_to_remote_name ||= credentials['push_to_remote_name']
+        instance.ticket_tracking_tool ||= credentials['ticket_tracking_tool'] # rubocop:disable Metrics/LineLength
+        instance.use_git_submodules ||= credentials['use_git_submodules']
+        instance.git_tool ||= credentials['git_tool']
+        instance.time_tracking_username ||= credentials['time_tracking_username']
+        instance.time_tracking_password ||= credentials['time_tracking_password']
+        instance.time_tracking_owner ||= credentials['time_tracking_owner']
+        instance.time_tracking_default_project_id ||= credentials['time_tracking_default_project_id'] # rubocop:disable Metrics/LineLength
+        instance.time_tracking_tool ||= credentials['time_tracking_tool']
+        instance.git_quiet ||= (credentials['git_quiet']) ? '&> /dev/null' : ''
+        instance.use_defaults ||= credentials['use_defaults']
+        instance.default_pull_request_title_format ||= credentials['default_pull_request_title_format'] # rubocop:disable Metrics/LineLength
+        instance.log_path ||= credentials['log_path']
+        instance.credentials_loaded_from ||= load_credentials_from
       end
 
-      def read_credenials(path = Client.config_path)
-        YAML.load_file(File.expand_path(load_credentials_from(path)))
+      def read_credenials
+        YAML.load_file(File.expand_path(load_credentials_from))
       end
 
-      def load_credentials_from(path = Client.config_path)
+      def load_credentials_from
         dir = Dir.pwd
-        credentials_loaded_from = ''
-        while File.expand_path(dir) != '/' do
-          unless File.exists?("#{dir}/.toolshedrc")
+        while File.expand_path(dir) != '/'
+          unless File.exist?("#{dir}/.toolshedrc")
             dir = File.join dir, '..'
             next
           end
--- lib/toolshed/commands/base.rb
+++ lib/toolshed/commands/base.rb
@@ -3,18 +3,19 @@ require 'optparse'
 
 module Toolshed
   module Commands
+    # Base class for all commands responsible for common methods
     class Base
-      def initialize(options={})
+      def initialize(_options = {})
       end
 
-      def self.parse(command, cli_options = {})
+      def self.parse(command, cli_options = {}) # rubocop:disable Metrics/AbcSize, Metrics/MethodLength, Metrics/LineLength, Metrics/CyclomaticComplexity
         options = {}
         options_parser = OptionParser.new do |opts|
           opts.banner = cli_options[:banner] if cli_options[:banner]
           cli_options[:options].each do |option_name, option_variables|
             letter_map = ('a'..'z').map { |letter| letter }
-            short_on = (option_variables[:short_on]) ? option_variables[:short_on] : letter_map[rand(letter_map.length)]
-            on = (option_variables[:on]) ? option_variables[:on] : "--#{option_name.to_s.split('_').join('-')} [ARG]"
+            short_on = (option_variables[:short_on]) ? option_variables[:short_on] : letter_map[rand(letter_map.length)] # rubocop:disable Metrics/LineLength
+            on = (option_variables[:on]) ? option_variables[:on] : "--#{option_name.to_s.split('_').join('-')} [ARG]" # rubocop:disable Metrics/LineLength
             opts.on(short_on, on) do |opt|
               value = (option_variables[:default].nil?) ? opt : option_variables[:default]
               options.merge!(option_name => value)
@@ -36,64 +37,64 @@ module Toolshed
         required = options[:required] || false
         value = ''
         if required
-          until !value.empty?
-            value = prompt_user_input(message, options)
-          end
+          value = prompt_user_input(message, options) while value.empty?
         else
           value = prompt_user_input(message, options)
         end
         value
       end
 
-      def read_user_input_title(message, options={})
-        return options[:title] if options.has_key?(:title)
+      def read_user_input_title(message, options = {})
+        return options[:title] if options.key?(:title)
         read_user_input(message, options)
       end
 
-      def read_user_input_body(message, options={})
-        return options[:body] if options.has_key?(:body)
+      def read_user_input_body(message, options = {})
+        return options[:body] if options.key?(:body)
         read_user_input(message, options)
       end
 
       def use_ticket_tracker_project_id(options)
-        options
-        options.merge!({
+        options.merge!(
           ticket_tracker_const: 'USE_PROJECT_ID',
           type: :project_id,
           default_method: 'default_pivotal_tracker_project_id',
-          default_message: "Project ID (Default: #{Toolshed::Client.instance.default_pivotal_tracker_project_id}):",
-        })
-        options = use_ticket_tracker_by_type(options)
+          default_message: "Project ID (Default: #{Toolshed::Client.instance.default_pivotal_tracker_project_id}):" # rubocop:disable Metrics/LineLength
+        )
+        use_ticket_tracker_by_type(options)
       end
 
       def use_ticket_tracker_project_name(options)
-        options.merge!({
+        options.merge!(
           ticket_tracker_const: 'USE_PROJECT_NAME',
           type: :project,
           default_method: 'default_ticket_tracker_project',
-          default_message: "Project Name (Default: #{Toolshed::Client.instance.default_ticket_tracker_project}):",
-        })
+          default_message: "Project Name (Default: #{Toolshed::Client.instance.default_ticket_tracker_project}):" # rubocop:disable Metrics/LineLength
+        )
         use_ticket_tracker_by_type(options)
       end
 
       def use_ticket_tracker_by_type(options)
-        use_field = Object.const_get("#{ticket_tracker_class}::#{options[:ticket_tracker_const]}") rescue false
+        use_field = Object.const_get("#{ticket_tracker_class}::#{options[:ticket_tracker_const]}") rescue false # rubocop:disable Metrics/LineLength, Style/RescueModifier
         if use_field
-          ticket_tracker_response = read_user_input(options[:default_message],
-            options.merge!({ default: Toolshed::Client.instance.send(options[:default_method]) })
+          ticket_tracker_response = read_user_input(
+            options[:default_message],
+            options.merge!(
+              default: Toolshed::Client.instance.send(options[:default_method])
+            )
           )
-          options.merge!({ options[:type] => ticket_tracker_response })
+          options.merge!(options[:type] => ticket_tracker_response)
         end
         options
       end
 
       def use_project_id
-        Object.const_get("#{ticket_tracker_class}::USE_PROJECT_ID") rescue false
+        Object.const_get("#{ticket_tracker_class}::USE_PROJECT_ID") rescue false # rubocop:disable Style/RescueModifier, Metrics/LineLength
       end
 
       private
 
-        def prompt_user_input(message, options)
+        def prompt_user_input(message, options) # rubocop:disable Metrics/AbcSize, Metrics/CyclomaticComplexity
           required = options[:required] || false
           puts message
           value = $stdin.gets.chomp
