9f3a60b01c9f0abb3eb50945bd74924c0cd48c12
Jake Waller <wallerjake@gmail.com>
2015-09-28 21:20:57 -0500

    Clean up the ssh class fix rubocop issues

--- lib/toolshed/server_administration/ssh.rb
+++ lib/toolshed/server_administration/ssh.rb
@@ -4,27 +4,29 @@ require 'net/ssh'
 
 module Toolshed
   module ServerAdministration
+    # SSH class that can ssh to a host and perform commands
     class SSH
-      attr_accessor :keys, :password, :sudo_password, :host, :user, :ssh_options, :channel, :commands, :password, :data
+      attr_accessor :channel, :commands, :data, :host, :keys, :password, :ssh_options, :sudo_password, :user # rubocop:disable LineLength
       attr_reader :silent
 
-      def initialize(options={})
-        self.password = options[:password] ||= ''
-        self.sudo_password = options[:sudo_password] ||= ''
-        self.keys = options[:keys] ||= ''
-        self.host = options[:host] ||= ''
-        self.user = options[:user] ||= ''
-        self.ssh_options = options[:ssh_options] ||= {}
-        self.commands = options[:commands] ||= ''
-        self.password = Toolshed::Password.new(options)
-        self.data = []
+      def initialize(options = nil) # rubocop:disable AbcSize, CyclomaticComplexity, PerceivedComplexity, LineLength
+        options ||= {}
+        @password = options[:password] || ''
+        @sudo_password = options[:sudo_password] || ''
+        @keys = options[:keys] || ''
+        @host = options[:host] || ''
+        @user = options[:user] || ''
+        @ssh_options = options[:ssh_options] || {}
+        @commands = options[:commands] || ''
+        @password = Toolshed::Password.new(options)
+        @data = []
         @silent = options[:silent] || false
 
         set_ssh_options
       end
 
       def execute
-        Net::SSH.start(self.host, self.user, self.ssh_options) do |ssh|
+        Net::SSH.start(host, user, ssh_options) do |ssh|
           ssh.open_channel do |channel|
             self.channel = channel
             request_pty
@@ -38,9 +40,10 @@ module Toolshed
       protected
 
         def run_commands
-          # @TODO fix this so it does not just pass in the commands option but converts to a string i.e. command1;command2
-          self.channel.exec(self.commands) do |ch, success|
-            abort "Could not execute commands!" unless success
+          # @TODO fix this so it does not just pass in the commands option
+          # Converts to string like command1;command2
+          channel.exec(commands) do |_ch, success|
+            abort 'Could not execute commands!' unless success
 
             on_data
             on_extended_data
@@ -49,25 +52,28 @@ module Toolshed
         end
 
         def request_pty
-          self.channel.request_pty do |ch, success|
-            puts (success) ? "Successfully obtained pty" : "Could not obtain pty" unless silent
+          channel.request_pty do |_ch, success|
+            unless silent
+              message = (success) ? 'Successfully obtained pty' : 'Could not obtain pty'
+              puts message
+            end
           end
         end
 
         def on_close
-          self.channel.on_close do |ch|
-            puts "Channel is closing!" unless silent
+          channel.on_close do |_ch|
+            puts 'Channel is closing!' unless silent
           end
         end
 
         def on_extended_data
-          self.channel.on_extended_data do |ch, type, data|
+          channel.on_extended_data do |_ch, _type, data|
             puts "stderr: #{data}" unless silent
           end
         end
 
         def on_data
-          self.channel.on_data do |ch, data|
+          channel.on_data do |_ch, data|
             puts "#{data}" unless silent
             self.data << data
             send_data(data)
@@ -76,20 +82,25 @@ module Toolshed
 
         def send_data(data)
           send_password_data if data =~ /password/
-          send_yes_no_data if data =~ /Do you want to continue \[Y\/n\]?/
+          # send_yes_no_data if data =~ /Do you want to continue \[Y\/n\]?/
+          send_yes_no_data if data =~ %r{Do you want to continue [Y/n]?}
         end
 
         def send_password_data
-          self.channel.send_data "#{@password.read_user_input_password('sudo_password')}\n"
+          channel.send_data "#{password.read_user_input_password('sudo_password')}\n"
         end
 
         def send_yes_no_data
-          self.channel.send_data "Y\n"
+          channel.send_data "Y\n"
         end
 
-        def set_ssh_options
-          self.ssh_options.merge!({ keys: [self.keys] }) unless keys.nil? || keys.empty?
-          self.ssh_options.merge!({ password: self.password.read_user_input_password('password') }) if keys.nil? || keys.empty?
+        def set_ssh_options # rubocop:disable AbcSize
+          if keys.nil? || keys.empty?
+            final_password = password.read_user_input_password('password')
+            ssh_options.merge!(password: final_password)
+          else
+            ssh_options.merge!(keys: [keys]) unless keys.nil? || keys.empty?
+          end
         end
     end
   end
