5a34899372370099821d537920c27c65b5f0b195
Jake Waller <wallerjake@gmail.com>
2015-10-31 15:09:29 -0500

    Fix the password translator
    It wasn't working well when the actual password was passed through. Fixed issues with mysql backup the password not working right. The fix for this was to make the -pthepass right next to each other no space between.

--- lib/toolshed/databases/mysql/backup.rb
+++ lib/toolshed/databases/mysql/backup.rb
@@ -29,12 +29,18 @@ module Toolshed
           raise TypeError, "Wait time passed in is not a number #{wait_time}" unless wait_time.is_a?(Fixnum)
           Toolshed.logger.info "Starting execution of mysqldump -h #{host} -u #{username} #{hidden_password_param} #{name} > #{path}."
           create_path
-          Toolshed::Base.wait_for_command("mysqldump -h #{host} -u #{username} #{password_param} #{name} > #{path}", wait_time)
+          results = Toolshed::Base.wait_for_command("mysqldump -h #{host} -u #{username} #{password_param} #{name} > #{path}", wait_time)
+          unless results[:stderr].is_a?(NilClass)
+            error_message = results[:stderr].join(' ')
+            Toolshed.logger.fatal error_message
+            raise Toolshed::PermissionsException, error_message
+          end
+          Toolshed.logger.info results[:stdout].join(' ') unless results[:stdout].is_a?(NilClass)
           Toolshed.logger.info 'mysqldump has completed.'
         end
 
         def password_param
-          password.nil? || password.empty?  ? '' : "-p #{password_from_config(password)}"
+          password.nil? || password.empty?  ? '' : "-p#{password_from_config(password)}"
         end
 
         def hidden_password_param
