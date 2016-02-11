0a29aa1f758ad21c4421e0f9a92ad48a5fcf0f6c
Jake Waller <wallerjake@gmail.com>
2016-02-10 21:06:10 -0600

    Allow SCP upload and download to pass in ssh_options
    This way a user can use their private key

--- lib/toolshed/server_administration/scp.rb
+++ lib/toolshed/server_administration/scp.rb
@@ -10,28 +10,30 @@ module Toolshed
     class SCP
       include Toolshed::Password
 
-      attr_reader :local_path, :password, :remote_host, :remote_path, :username, :verbose_output # rubocop:disable LineLength
+      attr_reader :local_path, :password, :remote_host, :remote_path, :ssh_options, :username, :verbose_output # rubocop:disable LineLength
 
       def initialize(options = nil)
         options ||= {}
-
         @password = options[:password]
         @remote_host = options[:remote_host]
         @remote_path = options[:remote_path]
         @local_path = options[:local_path]
         @username = options[:username]
         @verbose_output = options[:verbose_output]
+        @ssh_options = options[:ssh_options]
+        @ssh_options ||= {}
+        @ssh_options.merge(password: password_from_config(password))
       end
 
       def download
         Toolshed.logger.info "Attempting to SCP from #{username}@#{remote_host}:#{remote_path} to #{local_path}." # rubocop:disable LineLength
-        Net::SCP.download!(remote_host, username, remote_path, local_path, ssh: { password: password_from_config(password) }, recursive: true) # rubocop:disable LineLength
+        Net::SCP.download!(remote_host, username, remote_path, local_path, ssh: ssh_options, recursive: true) # rubocop:disable LineLength
         on_complete
       end
 
       def upload
         Toolshed.logger.info "Attempting to SCP from #{local_path} to #{username}@#{remote_host}:#{remote_path}." # rubocop:disable LineLength
-        Net::SCP.upload!(remote_host, username, local_path, remote_path, ssh: { password: password_from_config(password) }, recursive: true) # rubocop:disable LineLength
+        Net::SCP.upload!(remote_host, username, local_path, remote_path, ssh: ssh_options, recursive: true) # rubocop:disable LineLength
         on_complete
       end
 
